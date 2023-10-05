5335bd50eaa93e25d0d3f79086587f090cb3bcff
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-10-05 10:18:42 +0600

    Added Add Book page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import Home from './pages/Home/Home.jsx';
+import AddBook from './pages/AddBook/AddBook.jsx';
 
 const routes = createBrowserRouter([
 	{
@@ -13,6 +14,10 @@ const routes = createBrowserRouter([
 			{
 				path: '/',
 				element: <Home />,
+			},
+			{
+				path: '/addBook',
+				element: <AddBook />,
 			},
 		],
 	},
